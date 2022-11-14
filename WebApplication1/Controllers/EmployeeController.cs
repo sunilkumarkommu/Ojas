5f2142a7c7f6fe9686d30abae303794ed8f3e442
Kiran Panthangi <kp19274@corp.ojas-it.com>
2022-11-14 15:11:37 +0530

    added new files

---
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet(Name = "addEmployee")]
+        public string add()
+        {
+            string s = "hello world";
+            return s;
+
+        }
+    }
+}
